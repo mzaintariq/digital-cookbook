17735bab3cb1a6449591cf25bd12777c970d7161
Muhammad Zain Tariq <49867382+mzaintariq@users.noreply.github.com>
2026-01-31 01:09:10 +0500

    Update AppFooter.vue

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="py-4 text-center text-xs text-gray-500 shrink-0">
+  <footer class="py-8 md:py-4 text-center text-xs text-gray-500 shrink-0">
     © 2026, Muhammad Zain Tariq
   </footer>
 </template>
